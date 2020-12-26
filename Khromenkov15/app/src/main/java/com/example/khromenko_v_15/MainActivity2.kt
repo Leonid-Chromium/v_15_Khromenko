a5f3c98c32ace07bd9f2bd7d07b5367c13a04788
LeonidKhromenko <l.khromenko@gmail.com>
2020-12-26 19:40:50 +0700

    Create_all

---
+++ Khromenkov15/app/src/main/java/com/example/khromenko_v_15/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.khromenko_v_15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
